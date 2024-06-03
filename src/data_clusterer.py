d993621ba9686c3052accb32fd90db3477031e20
Joaquin Heffron <biboy@Beebs-MacBook-Air.local>
2024-06-03 12:00:02 +0800

    LDA script

--- src/data_clusterer.py
+++ src/data_clusterer.py
@@ -25,14 +25,21 @@ class DataClusterer:
         centers = kmeans.cluster_centers_
         inertia = kmeans.inertia_
 
-        return centers, y_kmeans, inertia
+        # if 1 in y_kmeans:
+
+        #     print(y_kmeans)
+        #     silhouette = silhouette_score(self.x, y_kmeans)
+            
+        # else: silhouette = []
+
+        return centers, y_kmeans, inertia #, silhouette
 
     def computeKMeansInertia(self, show_plot: bool=True):
         K = [i for i in range(1, self.max_clusters + 1)]
         inertias = []
 
         for i in range(1, self.max_clusters + 1):
-            _, _, inertia = self.computeKMeans(i) # TRAINING PART FOR EACH NUMBER OF CLUSTERS i
+            _, _, inertia, _ = self.computeKMeans(i) # TRAINING PART FOR EACH NUMBER OF CLUSTERS i
             inertias.append(inertia)
 
         if show_plot:
@@ -42,6 +49,22 @@ class DataClusterer:
             plt.show()
 
         return K, inertias
+    
+    # def computeKMeansSilhouette(self, show_plot: bool=True):
+    #     K = [i for i in range(1, self.max_clusters + 1)]
+    #     silhouettes = []
+
+    #     for i in range(1, self.max_clusters + 1):
+    #         _, _, _, silhouette = self.computeKMeans(i) # TRAINING PART FOR EACH NUMBER OF CLUSTERS i
+    #         silhouettes.append(silhouette)
+
+    #     if show_plot:
+    #         plt.plot(K, silhouettes, 'bx-')
+    #         plt.xlabel('K value')
+    #         plt.ylabel('Silhouette')
+    #         plt.show()
+
+    #     return K, silhouettes
 
     def getClusters(self, n_clusters: int, centers: list, features: list, lsa):
         original_space_centroids = lsa[0].inverse_transform(centers)
